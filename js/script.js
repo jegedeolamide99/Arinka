00050a149d048a6905c66b7a538d1e89c28b4bab
Jegede Olamide <jegedeolamide99@gmail.com>
2017-11-29 19:51:21 +0100

    Initiate and call cloudinary function
    added code that makes the image responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Cloudinary responsive method
-    var cl = cloudinary.Cloudinary.new({cloud_name: "harryola9"}); 
+    var cl = cloudinary.Cloudinary.new({cloud_name: "harryola9"});
+    cl.config({responsive_use_breakpoints:"resize"}) 
     cl.responsive();
 
